c7a4422291ea1c1a4de0c03122ba2463c06024fb
AadityaAcharya156 <131011277+AadiAcharya@users.noreply.github.com>
2026-02-15 20:47:23 +0545

    tailwind not setting up

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="flex bg-blue-500 h-screen items-center justify-center">
+      <div className="bg-red-500 p-6 text-white">One</div>
+      <div className="bg-green-500 p-6 text-white">Two</div>
+    </div>
+  );
+}
